e0b30c965b3c9e4765672cfd2080876806c66de8
Gustavo <43652968+Guscardoso@users.noreply.github.com>
2018-09-27 15:58:57 -0300

    Update gravity.py

--- gravity.py
+++ gravity.py
@@ -1,24 +1,25 @@
+#CALCULADORA GRAVITACIONAL
 print("""CALCULADORA GRAVITACIONAL:
-          a-gravidade de um planeta
-          b-força gravitacional entre
+          1-gravidade de um planeta
+          2-força gravitacional entre
           dois corpos
-          c-velocidade de um satélite  
+          3-velocidade de um satélite  
           obs: use unidades em kg e m""")
-x = input ("Escolha:")
-if x=="a":
+n = input ("Escolha:")
+if n==1:
    m = int (input  ("Massa do planeta:"))
    r = int (input  ("Raio do planeta:"))
    k = (6.7/(10**11))
    g =( (k*m)/(r**2))
    print("A aceleração é", g,"m/s^2")
-if x=="b":
+if n==2:
 	m1 = int (input  ("Massa 1:"))
 	m2 = int (input  ("Massa 2:"))
 	r = int (input  ("Distância entre as duas massas:"))
 	k = (6.7/(10**11))
 	f = ((k*m1*m2)/r**2)
 	print("A força é de ",f,"N")
-if x=="c":
+if n==3:
    print ("velocidade de um satélite na órbita terrestre")
    h = int ( input  ("Altura do do satélite :"))
    r = ((6.37)*(10**6))
